d2478e61ba415af897d2e67fb16877e01eb9473a
wesleyfilho <30578522+wesleyfilho@users.noreply.github.com>
2025-03-21 12:51:19 -0300

    Update main.py
    Adicionado  a licença

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-ma
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Nome do Programa: ExemploPython
+Copyright (C) 2025  O Teu Nome
+
+Este programa é software livre: podes redistribuí-lo e/ou modificá-lo
+nos termos da Licença Pública Geral GNU, conforme publicada pela Free Software Foundation,
+quer a versão 3 da Licença, quer (à tua escolha) qualquer versão posterior.
+
+Este programa é distribuído na esperança de que seja útil,
+mas SEM QUALQUER GARANTIA; sem mesmo a garantia implícita de
+COMERCIALIZAÇÃO ou ADEQUAÇÃO A UM DETERMINADO PROPÓSITO. Consulta a
+Licença Pública Geral GNU para mais detalhes.
+
+Deverás ter recebido uma cópia da Licença Pública Geral GNU
+junto com este programa. Se não, vê <https://www.gnu.org/licenses/>.
+"""
+
+
